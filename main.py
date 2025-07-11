ed7d3d5eadf473f2c9693d2fbf38908784769e86
thegrimcoder <mbrust035@gmail.com>
2025-07-11 18:48:23 -0500

    Set up the game display, changed the tick speed to 60

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    time_clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        dt = time_clock.tick(60)/1000
+
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
