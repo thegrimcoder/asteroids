e09d21f88861708e2173dc9430ab0ef89957fecb
thegrimcoder <mbrust035@gmail.com>
2025-07-11 20:10:31 -0500

    Create asteroid.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     time_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         dt = time_clock.tick(60)/1000
 
